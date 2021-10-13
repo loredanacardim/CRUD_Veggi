f94ae58524604c3243ed90fa6a6cc79e1cef349b
Loredana Cardim <loredanacardim@outlook.com>
2021-10-13 15:08:27 -0300

    commit inicial

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Controle de Tarefa dos Usuários">
+        <div className='display-4'>Bem Vindo!!</div>
+        <hr />
+        <p className="mb-0">Sistema para Cadastro de Usuários e Tarefas</p>
+    </Main>
